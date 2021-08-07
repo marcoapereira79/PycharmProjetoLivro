71f423728b4d094cfff9358bb044ca86d438dbf0
Marco A Pereira <marcoapereira79@gmail.com>
2021-08-07 18:48:14 -0300

    Botão de inicio e controle de estatistica do jogo

---
+++ alien_invasion/button.py
@@ -0,0 +1,35 @@
+import pygame.font
+
+class Button():
+
+    def __init__(self, ai_configuracoes, tela, msg):
+        """Inicializa os atributos do botão"""
+        self.tela = tela
+        self.tela_rect = tela.get_rect()
+
+        # Define as dimensões e as propriedades do botão
+        self.width, self.height = 200, 50
+        self.button_color = (115, 0, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None,48)
+
+        # Constrói o objeto rect do botão e o centraliza
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.tela_rect.center
+
+        # A mensagem do botão deve ser preparada apenas uma vez
+        self.prep_msg(msg)
+
+
+    def prep_msg(self,msg):
+        """Transforma msg em imagem renderizada e centraliza o texto no botão."""
+        self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+
+    def draw_button(self):
+        # Desenha  um botão em branco e , em seguida desenha a mensagem
+        self.tela.fill(self.button_color,self.rect)
+        self.tela.blit(self.msg_image, self.msg_image_rect)
+
--- alien_invasion/invasão_alien.py
+++ alien_invasion/invasão_alien.py
@@ -2,14 +2,23 @@ import pygame
 from pygame.sprite import Group
 from configuracoes import Configuracoes
 from nave import Nave
+from estat_game import GameStats
 import jogo_funcoes as jf
+from button import Button
 
 
 def run_game():
     pygame.init()
+    # objeto das Configurações do jogo
     ai_configuracoes = Configuracoes()
+    # Tela do jogo
     tela = pygame.display.set_mode((ai_configuracoes.tela_width, ai_configuracoes.tela_height))
+    # Nome da Tela
     pygame.display.set_caption("Space War")
+    # Cria o botão Play
+    play_button = Button(ai_configuracoes, tela, "Play")
+    # Cria uma instância para armazenar dados estatíticos do jogo
+    estatistica = GameStats(ai_configuracoes)
     # Cria uma espaçonave
     nave = Nave(ai_configuracoes, tela)
     # Cria um grupo no qual armazeno os projéteis
@@ -21,11 +30,15 @@ def run_game():
 
     #Inicia o laço principal do jogo
     while True:
-        jf.check_eventos(ai_configuracoes, tela, nave, municoes)
-        nave.update_nave()
-        jf.update_municoes(ai_configuracoes, tela, nave, aliens, municoes)
-        jf.update_aliens(ai_configuracoes, aliens)
-        jf.update_tela(ai_configuracoes, tela, nave, aliens, municoes)
+        jf.check_eventos(ai_configuracoes, tela, estatistica, play_button, nave, aliens, municoes)
+        # Só xecutará se o game estiver ativo (game inativo se as vidas terminarem)
+        if estatistica.game_active:
+            nave.update_nave()
+            jf.update_municoes(ai_configuracoes, tela, nave, aliens, municoes)
+            jf.update_aliens(ai_configuracoes,estatistica, tela, nave, aliens, municoes)
+
+        jf.update_tela(ai_configuracoes, estatistica ,tela, nave, aliens, municoes, play_button)
+
 
 
 run_game()
